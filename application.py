0406c688114b76e7d754cbea3a66da09aa1dbcbf
Curtis Robords <curtis.robords@gmail.com>
2022-11-29 21:11:47 -0500

    adding webhook

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 from hello_world import hello
 import os
+import git
 from flask import Flask, jsonify, Response, render_template, request
 
 application = Flask(__name__)
@@ -11,7 +12,17 @@ def index():
     except KeyError:
         endpoint = 'Local'
     return hello(environment=endpoint)
-
+=
+app = Flask(__name__)
+@app.route('/update_server', methods=['POST'])
+    def webhook():
+        if request.method == 'POST':
+            repo = git.Repo('path/to/git_repo')
+            origin = repo.remotes.origin
+origin.pull()
+return 'Updated PythonAnywhere successfully', 200
+        else:
+            return 'Wrong event type', 400
 
 if __name__ == '__main__':
     application.run(host='127.0.0.1', port=8080, debug=True)
