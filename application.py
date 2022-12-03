40810d7dba4b81fee1bacd8adf9df887de796c47
Curtis Robords <curtis.robords@gmail.com>
2022-12-03 16:35:34 -0500

    test

--- application.py
+++ application.py
@@ -16,7 +16,7 @@ def index():
 @application.route('/update_server', methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        repo = git.Repo('~/always_free') 
+        repo = git.Repo('.', search_parent_directories=True) 
         origin = repo.remotes.origin
         origin.pull()
         return 'Updated PythonAnywhere successfully', 200
